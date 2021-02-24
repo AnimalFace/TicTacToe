9e1fadb4e05901b61a7c24cda719cde3c4d57b99
Jason Horn <jasonh.horn1@gmail.com>
2021-02-24 11:17:56 -0800

    implement board space logic

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,18 +6,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      turn: playerOne,
-
-
+      turn: 'x',
+      board: [[null, null, null],[null, null, null],[null, null, null]],
+      winner: null
     };
-
+    this.renderSpot = this.renderSpot.bind(this);
   }
 
   componentDidMount() {
 
   }
 
+  renderSpot(coordinate, board, turn) {
+    let newBoard = JSON.parse(JSON.stringify(board));
+    if (board[coordinate[0]][coordinate[1]] === null) {
+      newBoard[coordinate[0]][coordinate[1]] = turn;
+      this.setState({
+        board: newBoard
+      })
+      if (turn === 'x') {
+        this.setState({turn: 'o'})
+      }
+      if (turn === 'o') {
+        this.setState({turn: 'x'})
+      }
+    }
+  }
+
   render() {
+    const { turn, board, winner } = this.state;
 
     return (
       <Styles>
